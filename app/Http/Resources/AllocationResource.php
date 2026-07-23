1af6a5fad030891d212f0221063730748277b9b2
AhmadArifff <aa2851214@gmail.com>
2026-07-23 15:16:53 +0700

    V.1.1.2 Fix Show Data sesuai user login

--- app/Http/Resources/AllocationResource.php
+++ app/Http/Resources/AllocationResource.php
@@ -17,7 +17,9 @@ class AllocationResource extends JsonResource
             'date' => $this->date->toDateString(),
             'note' => $this->note,
             'attachment_path' => $this->attachment_path,
-            'attachment_url' => $this->when($this->attachment_path, Storage::disk('supabase')->url($this->attachment_path)),
+            'attachment_url' => $this->when($this->attachment_path, function () {
+                return Storage::disk('supabase')->url($this->attachment_path);
+            }),
             'created_at' => $this->created_at,
         ];
     }
--- app/Http/Resources/ExpenseResource.php
+++ app/Http/Resources/ExpenseResource.php
@@ -20,7 +20,9 @@ class ExpenseResource extends JsonResource
             'is_estimate' => (bool) $this->is_estimate,
             'note' => $this->note,
             'attachment_path' => $this->attachment_path,
-            'attachment_url' => $this->when($this->attachment_path, Storage::disk('supabase')->url($this->attachment_path)),
+            'attachment_url' => $this->when($this->attachment_path, function () {
+                return Storage::disk('supabase')->url($this->attachment_path);
+            }),
             'created_at' => $this->created_at,
         ];
     }
